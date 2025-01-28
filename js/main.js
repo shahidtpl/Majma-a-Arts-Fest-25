d7d84f2cd55629242e74ed9caef065a87785c4af
Shahid Thottupoyil <mskmskshahid@gmail.com>
2025-01-28 19:50:08 +0530

    final file

---
+++ js/main.js
@@ -0,0 +1,96 @@
+jQuery(function($) {'use strict',
+	
+	//Countdown js
+	 $("#countdown").countdown({
+			date: "19 january 2024 12:00:00",
+			format: "on"
+		},
+		
+		function() {
+			// callback function
+		});
+	
+
+	
+	//Scroll Menu
+
+	function menuToggle()
+	{
+		var windowWidth = $(window).width();
+
+		if(windowWidth > 767 ){
+			$(window).on('scroll', function(){
+				if( $(window).scrollTop()>405 ){
+					$('.main-nav').addClass('fixed-menu animated slideInDown');
+				} else {
+					$('.main-nav').removeClass('fixed-menu animated slideInDown');
+				}
+			});
+		}else{
+			
+			$('.main-nav').addClass('fixed-menu animated slideInDown');
+				
+		}
+	}
+
+	menuToggle();
+	
+	
+	// Carousel Auto Slide Off
+	$('#speakers, #links-feed, #sponsor-carousel ').carousel({
+		interval: false
+	});
+
+
+	// Contact form validation
+	var form = $('.contact-form');
+	form.submit(function () {'use strict',
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
+
+	$( window ).resize(function() {
+		menuToggle();
+	});
+
+	$('.main-nav ul').onePageNav({
+		currentClass: 'active',
+	    changeHash: false,
+	    scrollSpeed: 900,
+	    scrollOffset: 0,
+	    scrollThreshold: 0.3,
+	    filter: ':not(.no-scroll)'
+	});
+
+});
+
+
+// Google Map Customization
+(function(){
+
+	var map;
+
+	map = new GMaps({
+		el: '#gmap',
+		lat: 43.04446,
+		lng: -76.130791,
+		scrollwheel:false,
+		zoom: 16,
+		zoomControl : false,
+		panControl : false,
+		streetViewControl : false,
+		mapTypeControl: false,
+		overviewMapControl: false,
+		clickable: false
+	});
+
+// 	
+
+// 	map.setStyle("map_style");
+}());
+
+
+
